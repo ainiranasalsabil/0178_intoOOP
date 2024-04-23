38040d24447454504bed2fb65da719074fcbee8e
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-04-23 11:19:28 +0700

    Baris ini menggunakan metode hitungLuas() yang telah dibahas sebelumnya untuk menghitung luas persegi panjang. Hasil perhitungan kemudian ditampilkan dengan pesan "Luasnya =" ke konsol.

--- BangunDatar/BangunDatar.cpp
+++ BangunDatar/BangunDatar.cpp
@@ -22,5 +22,7 @@ public:
 	void display() {
 		cout << "Panjangnya = " << panjang << endl;
 		cout << "Lebarnya = " << lebar << endl;
-		
-}
+		cout << "Luasnya = " << hitungLuas() << endl;
+	}
+};
+
